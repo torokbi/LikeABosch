2e6f50ab5a7491bfdca0b2fd888b9633664a257a
torokbi <bence.torok50@gmail.com>
2022-09-30 22:23:33 +0200

    Create class of 343.csv and 340.csv

--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 import csv
 
 cars = [] #the datas of one car
+gps = []
+distance = []
 
 class Car:
     def __init__(self, t, axv, ayv, psidtopt, absreftime, vxv, vyv):
@@ -11,6 +13,20 @@ class Car:
         self.absreftime = absreftime
         self.vxv = vxv
         self.vyv = vyv
+class GPS:
+    def __init__(self, t, Hunter_GPS_Mode):
+        self.t = t
+        self.Hunter_GPS_Mode = Hunter_GPS_Mode
+class Distace:
+    def __init__(self, t, Long_Delta_Distance, Long_Delta_Velocity):
+        self.t = t
+        self.Long_Delta_Distance = Long_Delta_Distance
+        self.Long_Delta_Velocity = Long_Delta_Velocity
+class
+
+"""
+Import of Group_416.csv
+"""
 
 with open("./SW Challenge 2022 - dataset/PSA_ADAS_W3_FC_2022-09-01_14-49_0054.MF4/Group_416.csv") as file416:
     csvfile = csv.reader(file416)
@@ -18,3 +34,77 @@ with open("./SW Challenge 2022 - dataset/PSA_ADAS_W3_FC_2022-09-01_14-49_0054.MF
     for row in csvfile:
         car = Car(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
         cars.append(car)
+with open("./SW Challenge 2022 - dataset/PSA_ADAS_W3_FC_2022-09-01_15-03_0057.MF4/Group_416.csv") as file416:
+    csvfile = csv.reader(file416)
+    next(csvfile)
+    for row in csvfile:
+        car = Car(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
+        cars.append(car)
+with open("./SW Challenge 2022 - dataset/PSA_ADAS_W3_FC_2022-09-01_15-12_0059.MF4/Group_416.csv") as file416:
+    csvfile = csv.reader(file416)
+    next(csvfile)
+    for row in csvfile:
+        car = Car(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
+        cars.append(car)
+with open("./SW Challenge 2022 - dataset/PSA_ADAS_W3_FC_2022-09-01_15-17_0060.MF4/Group_416.csv") as file416:
+    csvfile = csv.reader(file416)
+    next(csvfile)
+    for row in csvfile:
+        car = Car(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
+        cars.append(car)
+'''
+Import of Group_340.csv
+'''
+with open("./SW Challenge 2022 - dataset/PSA_ADAS_W3_FC_2022-09-01_14-49_0054.MF4/Group_340.csv") as file340:
+    csvfile = csv.reader(file340)
+    next(csvfile)
+    for row in csvfile:
+        gps_data = GPS(row[0], row[1])
+        gps.append(gps_data)
+with open("./SW Challenge 2022 - dataset/PSA_ADAS_W3_FC_2022-09-01_15-03_0057.MF4/Group_340.csv") as file340:
+    csvfile = csv.reader(file340)
+    next(csvfile)
+    for row in csvfile:
+        gps_data = GPS(row[0], row[1])
+        gps.append(gps_data)
+with open("./SW Challenge 2022 - dataset/PSA_ADAS_W3_FC_2022-09-01_15-12_0059.MF4/Group_340.csv") as file340:
+    csvfile = csv.reader(file340)
+    next(csvfile)
+    for row in csvfile:
+        gps_data = GPS(row[0], row[1])
+        gps.append(gps_data)
+with open("./SW Challenge 2022 - dataset/PSA_ADAS_W3_FC_2022-09-01_15-17_0060.MF4/Group_340.csv") as file340:
+    csvfile = csv.reader(file340)
+    next(csvfile)
+    for row in csvfile:
+        gps_data = GPS(row[0], row[1])
+        gps.append(gps_data)
+
+'''
+Import of Group_343.csv
+'''
+
+with open("./SW Challenge 2022 - dataset/PSA_ADAS_W3_FC_2022-09-01_14-49_0054.MF4/Group_343.csv") as file343:
+    csvfile = csv.reader(file343)
+    next(csvfile)
+    for row in csvfile:
+        distance_data = Distace(row[0], row[1], row[2])
+        distance.append(distance_data)
+with open("./SW Challenge 2022 - dataset/PSA_ADAS_W3_FC_2022-09-01_15-03_0057.MF4/Group_343.csv") as file343:
+    csvfile = csv.reader(file343)
+    next(csvfile)
+    for row in csvfile:
+        distance_data = Distace(row[0], row[1], row[2])
+        distance.append(distance_data)
+with open("./SW Challenge 2022 - dataset/PSA_ADAS_W3_FC_2022-09-01_15-12_0059.MF4/Group_343.csv") as file343:
+    csvfile = csv.reader(file343)
+    next(csvfile)
+    for row in csvfile:
+        distance_data = Distace(row[0], row[1], row[2])
+        distance.append(distance_data)
+with open("./SW Challenge 2022 - dataset/PSA_ADAS_W3_FC_2022-09-01_15-17_0060.MF4/Group_343.csv") as file343:
+    csvfile = csv.reader(file343)
+    next(csvfile)
+    for row in csvfile:
+        distance_data = Distace(row[0], row[1], row[2])
+        distance.append(distance_data)
